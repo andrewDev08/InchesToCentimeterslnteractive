1281921b187dbce367e2dc4b8fcba62c6d14c88e
andrewDev08 <143661891+andrewDev08@users.noreply.github.com>
2023-09-10 09:56:01 +0000

    I messed up by editing the wrong file

--- main.cs
+++ main.cs
@@ -3,11 +3,18 @@ using System;
 class Program {
   public static void Main (string[] args) {
     double conversionRate = 2.54;
-    int inchValue = 5;
     
-    double centiValue = inchValue * conversionRate;
-    
-    Console.WriteLine("{0} Inches", inchValue );
-    Console.WriteLine("{0} Centimeters", centiValue );
+    Console.WriteLine("Please enter the number of inches: ");
+    var x = Console.ReadLine();
+
+    int inchValue;
+    if (int.TryParse(x, out inchValue))
+    {
+      double centiValue = inchValue * conversionRate;
+      Console.WriteLine("{0} Centimeters", centiValue );
+    }
+    else {
+      Console.WriteLine("Please only put the number" );
+    }
   }
 }
